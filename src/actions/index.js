d0e20e3ee7abf27071f6974017911b2d893b5d9d
pinoytech <forum.providence@yahoo.com>
2017-03-04 12:02:12 +0800

    Creating An Action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,11 @@
 export function selectBook(book) {
-    console.log('a book has been selected', book.title);
+  // console.log('a book has been selected', book.title);
+  // selectBook is an Action Creator. It needs to return an action,
+  // an object with a type property(usually uppercase). If not, you will receive the following error
+  // Uncaught Error: Actions may not have an undefined "type" property. Have you misspelled a constant?
+  // It sometimes contain a payload
+  return {
+    return: 'BOOK_SELECTED',
+    payload: book
+  };
 }
